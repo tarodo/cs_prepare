18976c44ed15cefb374d0cbc9d395472cf4e621a
Roman Volozhanin <rsvolozhanin@gmail.com>
2023-01-03 18:15:27 +0100

    add bfs average level

---
+++ Algorithms/Trees/08_bfs_average_level.py
@@ -0,0 +1,30 @@
+
+
+from Algorithms.Trees.base import create_bst
+
+
+def traverse(root):
+    result = []
+    from statistics import mean
+    from collections import deque
+    queue = deque()
+    queue.append(root)
+    while queue:
+        cur_level = []
+        for _ in range(len(queue)):
+            el = queue.popleft()
+            cur_level.append(el.data)
+            if el.left:
+                queue.append(el.left)
+            if el.right:
+                queue.append(el.right)
+
+        result.append(mean(cur_level))
+
+    return result
+
+
+arr = [100, 50, 200, 25, 75, 350]
+root = create_bst(arr)
+
+print(traverse(root))
