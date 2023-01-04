f85be39b8518472c9270ede1a11946628de542a3
Roman Volozhanin <rsvolozhanin@gmail.com>
2023-01-04 19:25:58 +0300

    add bfs level connection

---
+++ Algorithms/Trees/11_bfs_connect level.py
@@ -0,0 +1,59 @@
+from __future__ import print_function
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right, self.next = None, None, None
+
+    # level order traversal using 'next' pointer
+    def print_level_order(self):
+        nextLevelRoot = self
+        while nextLevelRoot:
+            current = nextLevelRoot
+            nextLevelRoot = None
+            while current:
+                print(str(current.val) + " ", end='')
+                if not nextLevelRoot:
+                    if current.left:
+                        nextLevelRoot = current.left
+                    elif current.right:
+                        nextLevelRoot = current.right
+                current = current.next
+            print()
+
+
+def connect_level_order_siblings(root):
+    q = deque()
+    q.append(root)
+    while q:
+        len_level = len(q)
+        prev = None
+        for _ in range(len_level):
+            el = q.popleft()
+            if prev:
+                prev.next = el
+            prev = el
+            if el.left:
+                q.append(el.left)
+            if el.right:
+                q.append(el.right)
+
+    return
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    connect_level_order_siblings(root)
+
+    print("Level order traversal using 'next' pointer: ")
+    root.print_level_order()
+
+
+main()
