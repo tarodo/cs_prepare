8e589377a7a426df2c6da78fe9ad6d439ea1740a
Roman Volozhanin <rsvolozhanin@gmail.com>
2023-02-19 10:49:23 +0100

    Add mirror for bt

---
+++ Algorithms/Trees/19_mirror.py
@@ -0,0 +1,25 @@
+from Algorithms.Trees.base import display_level_order, create_bst
+
+
+def mirror_tree(root):
+    if not root:
+        return
+
+    if root.left:
+        mirror_tree(root.left)
+
+    if root.right:
+        mirror_tree(root.right)
+
+    root.left, root.right = root.right, root.left
+
+    return root
+
+
+arr = [100, 50, 200, 25, 75, 125, 350]
+root = create_bst(arr)
+
+display_level_order(root)
+mirror_tree(root)
+print("\nMirrored Level Order Traversal:")
+display_level_order(root)
